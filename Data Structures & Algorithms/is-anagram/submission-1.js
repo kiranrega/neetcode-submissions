879d79f6bc8c518b1ae05802f427c50b8ea8c8c8
Kiran Kumar Rega <57248976+kiranrega@users.noreply.github.com>
2026-08-22 22:46:31 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+
+        let map = new Map();
+
+        for (let char of s) {
+            map.set(char, (map.get(char) || 0) + 1);
+        }
+
+        for (let char of t) {
+            if (!map.has(char)) return false;
+
+            map.set(char, map.get(char) - 1);
+
+            if (map.get(char) === 0) {
+                map.delete(char);
+            }
+        }
+
+        return map.size === 0;
+    }
+}
